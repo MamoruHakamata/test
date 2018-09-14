e2cd9a0ea4d4e089214f3c5ad75d553fcc113017
MamoruHakamata <hakamata@kaze-aruki.com>
2018-09-14 16:58:12 +0900

    デバッグ用UI追加

---
+++ DebugUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class DebugUI : MonoBehaviour {
+    [SerializeField]
+    private Text textArea = null;
+
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        MotorCycleController ctrl = GameSceneSettingManager.Instance.GetMortorcycleController();
+        string data = "";
+        if(ctrl != null)
+        {
+            data = string.Format("Target Speed = {0}\nCurrent Speed = {1}", ctrl.TargetMoveSpeed, ctrl.CurrentMoveSpeed);   
+        }
+        textArea.text = data;
+    }
+
+}
